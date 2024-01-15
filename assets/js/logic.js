223fbd6c93d38c813c744dd4f4951f75f048494a
Emily Kent <embow@Emilys-MacBook-Pro.local>
2024-01-15 13:52:45 +0000

    updated showQuestion function and correctAnswer function so next question appears and game ends

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,87 +1,76 @@
-
-var timer = document.querySelector(".timer")
-var questions = document.querySelector("#question-title")
-var choice = document.querySelector("#choices")
-var starterScreen = document.querySelector("#start-screen")
-var startButton = document.querySelector("#start")
-var submitButton = document.querySelector("#submit")
+var timer = document.querySelector(".timer");
+var questions = document.querySelector("#questions");
+var questionTitle = document.querySelector("#question-title");
+var choice = document.querySelector("#choices");
+var starterScreen = document.querySelector("#start-screen");
+var startButton = document.querySelector("#start");
+var submitButton = document.querySelector("#submit");
+var endScreen = document.querySelector("#end-screen");
 
 var secondsLeft = 60;
 //function for timer countdown to start when start button clicked
 function startTimer() {
-var timeInterval = setInterval(function() {
-
-    if(secondsLeft > 0) {
-    timer.textContent = secondsLeft
-    secondsLeft--;
-    
+  var timeInterval = setInterval(function () {
+    if (secondsLeft > 0) {
+      timer.textContent = secondsLeft;
+      secondsLeft--;
+    } else {
+      timer.textContent = "";
+      clearInterval(timeInterval);
+      //displayMessage()
+    }
+  }, 1000);
 }
-else {
-    timer.textContent = ""
-    clearInterval(timeInterval);
-    //displayMessage()
-}   
-}, 1000);
-} 
 
 //function to show hidden questions
+function unhideQuestions() {
+    starterScreen.classList.add("hide");
+    questions.classList.remove("hide");
+}
 
-function unhideQuestions () {
-    var hidden = document.querySelector(".hide")
-    if (hidden.style.display === "none") {
-        starterScreen.remove();
-        hidden.style.display = "block"
-    }
-    else {
-        hidden.style.display = "none"
-    }
-    }
-
-startButton.addEventListener("click", unhideQuestions)
-startButton.addEventListener("click", startTimer)
-
+startButton.addEventListener("click", unhideQuestions);
+startButton.addEventListener("click", startTimer);
 
 //loop through questions
-var score = 0
+var score = 0;
+var currentQuestion = 0;
 
-function showQuestions () {
-questions.textContent = quizQuestions[0].question;
+function showQuestions() {
+  questionTitle.textContent = quizQuestions[currentQuestion].question;
 
-//quizQuestions.choices.forEach()
-var choose = quizQuestions[0].choices
-for (let i = 0; i< choose.length; i++) {
-var optionButton = document.createElement("button")
-optionButton.textContent = choose[i]
-choice.appendChild(optionButton)
+  //quizQuestions.choices.forEach()
+  var choose = quizQuestions[currentQuestion].choices;
+  for (let i = 0; i < choose.length; i++) {
+    var optionButton = document.createElement("button");
+    optionButton.textContent = choose[i];
+    choice.appendChild(optionButton);
+    optionButton.addEventListener("click", correctAnswer);
+  }
 }
-
 //if statement for when an option button is clicked
-optionButton.addEventListener("click", correctAnswer)
 
-function correctAnswer(){
-if (quizQuestions[0].answer === quizQuestions[0].choices){
-    score +=5 
-    choice.textContent="Correct!"
+function correctAnswer(event) {
+  if (quizQuestions[currentQuestion].answer === event.target.textContent) {
+    score += 5;
+    choice.textContent = "Correct!";
+  } else {
+    secondsLeft -= 10;
+    choice.textContent = "Incorrect!";
+  }
+  currentQuestion++;
+  if (currentQuestion < 5) {
+    showQuestions();
+  } else {
+    gameOver();
+  }
 }
-else{
-    secondsLeft -=10
-    choice.textContent="Incorrect!"
-}
-}
-} 
 
-showQuestions()
+showQuestions();
 
 function gameOver() {
-    //if get to end of questions length show score page using the unhideScore function
-    //else time runs out show score page using the unhideScore function
-}
+    questions.classList.add("hide");
+    endScreen.classList.remove("hide");
+  }
 
-function unhideScore(){
 
-}
 
-    //submitButton.addEventListener("click", submit)
-function submit(){
-    //when button is clicked score and initials are logged to the local storage and appear in the highscore page
-}
